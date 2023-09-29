6cfb83d163f7e83a87c9956249d8ea559751bbd1
Kays8 <thirayut1379@gmail.com>
2023-09-29 22:44:36 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,17 @@ function App() {
   }
   return (
     <div>
+      <div className="info">
       <h1>Workshop 1 : Birthday List Project</h1>
-      <h2>Thirayut Boonin</h2>
+      <h1>Thirayut Boonin</h1>
+      </div>
       <main>
         <section className="container">
+          <div>
           <h3>{people.length} birthdays today</h3>
           <List people={people} />
           <button onClick={clearAll}>Clear All</button>
+          </div>
         </section>
       </main>
     </div>
